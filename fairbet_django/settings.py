bbdd085534c0850afd08dab14a9eb440f95de5af
mayank <mayank.infograins@gmail.com>
2022-06-15 19:41:51 +0530

    requirement.txt

--- fairbet_django/settings.py
+++ fairbet_django/settings.py
@@ -127,14 +127,15 @@ TIME_ZONE = 'Asia/Kolkata'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
-# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, '../fairbet_react/build/static'),
-# ]
