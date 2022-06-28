f33eeadbeca5448691465fc9b4b713c7cf7a1c04
mayank <mayank.infograins@gmail.com>
2022-06-28 11:14:05 +0530

    add request.data on payment/views

--- payment/urls.py
+++ payment/urls.py
@@ -3,7 +3,7 @@ from .views import payment_home, order_payment, callback, RazorPayOrderForm
 
 urlpatterns = [
     path('payment/',payment_home),
-    # path("order_payment/", order_payment, name="payment"),
+    path("order_payment/", order_payment, name="payment"),
     path("callback/", callback, name="callback"),
     path('razorPay/',RazorPayOrderForm.as_view()),
 ]
