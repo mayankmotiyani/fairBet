0022e4272bbbee5a7c382544ff85702871b1c387
mayank <mayank.infograins@gmail.com>
2022-07-01 17:32:25 +0530

    get api orderPlaced

--- order_placed/serializers.py
+++ order_placed/serializers.py
@@ -7,3 +7,13 @@ class BettingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Betting
         fields = "__all__"
+
+    
+    def to_representation(self, obj):
+        instance = super(BettingSerializer, self).to_representation(obj)
+        if instance['status'] == "B":
+            instance['status'] = "BACK"
+        elif instance['status'] == "L":
+            instance['status'] = "LAY"
+        
+        return instance
