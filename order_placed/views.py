fafde41d943a7f7c8b03d5d21244684bec29d086
mayank <mayank.infograins@gmail.com>
2022-07-01 15:26:16 +0530

    new app add order_placed

--- order_placed/views.py
+++ order_placed/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework_simplejwt.backends import TokenBackend
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 
