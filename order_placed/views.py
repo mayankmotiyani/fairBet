125cfec4297d4395bbb72517de28b2acaa9157de
mayank <mayank.infograins@gmail.com>
2022-07-04 11:52:19 +0530

    handle insufficient wallet exception

--- order_placed/views.py
+++ order_placed/views.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.backends import TokenBackend
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 import json
 from fairbet_auth_app.models import (
     Profile
@@ -44,26 +45,42 @@ class BettingOrderAPI(APIView):
             return Response(context,status = status.HTTP_401_UNAUTHORIZED)
         get_json = request.data['bet_json']
         get_json = json.loads(get_json)
-        get_json['user'] = get_logged_in_user_profile.id
-        betting_instance = Betting.objects.create(
-            user_id = get_json['user'],
-            amount = get_json['placeAmount'],
-            bet_on_team = get_json['betOnTeam'],
-            status = get_json['orderStatus'],
-            odds = get_json['Odds'],
-            match = get_json['liveMatch']
-        )
-        betting_instance.save()
-        """ taking wallet instance """
-        wallet_instance = Wallet.objects.get(user_id=get_logged_in_user_profile.id)
-        wallet_instance.amount -= betting_instance.amount
-        wallet_instance.save()
-        context = {
-            "status":status.HTTP_201_CREATED,
-            "response":"Successfully Placed Order and Wallet Updated!"
-        }
-        return Response(context,status = status.HTTP_201_CREATED)
-
+        """ here we will check your wallet balance """
+        try:
+            wallet_instance = get_object_or_404(Wallet,user_id=get_logged_in_user_profile.id)
+            get_wallet_amount = wallet_instance.amount
+            if get_json['placeAmount'] > get_wallet_amount and get_json['placeAmount'] != get_wallet_amount:
+                context = {
+                    "status":status.HTTP_400_BAD_REQUEST,
+                    "data":"Insufficient credit limit"
+                }
+                return Response(context,status=status.HTTP_400_BAD_REQUEST)
+            else:
+                get_json['user'] = get_logged_in_user_profile.id
+                betting_instance = Betting.objects.create(
+                    user_id = get_json['user'],
+                    amount = get_json['placeAmount'],
+                    bet_on_team = get_json['betOnTeam'],
+                    status = get_json['orderStatus'],
+                    odds = get_json['Odds'],
+                    match = get_json['liveMatch']
+                )
+                betting_instance.save()
+                """ taking wallet instance """
+                wallet_instance = Wallet.objects.get(user_id=get_logged_in_user_profile.id)
+                wallet_instance.amount -= betting_instance.amount
+                wallet_instance.save()
+                context = {
+                    "status":status.HTTP_201_CREATED,
+                    "response":"Successfully Placed Order and Wallet Updated!"
+                }
+                return Response(context,status = status.HTTP_201_CREATED)
+        except Exception as exception:
+            context = {
+                "status":status.HTTP_400_BAD_REQUEST,
+                "data":str(exception)
+            }
+            return Response(context,status = status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, *args, **kwargs):
         try:
