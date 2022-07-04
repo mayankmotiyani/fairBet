64950963ecae7b54ce4a11cf5244c9298fa0ecb9
mayank <mayank.infograins@gmail.com>
2022-07-04 19:22:26 +0530

    add automate updation on helpers python file

--- order_placed/helpers.py
+++ order_placed/helpers.py
@@ -3,8 +3,7 @@ import pandas as pd
 import numpy as np
 from django_pandas.io import read_frame
 from .models import Betting
-
-
+from django.db.models import F, Q, When, Value
 
 def handle_all_bettings(matchName,winningTeam):
     """ will filter according to match_name """
@@ -12,18 +11,30 @@ def handle_all_bettings(matchName,winningTeam):
     df['winning_team'] = winningTeam
 
     def CalculateLossProfit_userBetting(row,winningTeam):
-    if row.status == "BACK" and row.bet_on_team == winningTeam and row.winning_team == winningTeam:
-        return row.amount * row.odds - row.amount 
-    elif row.status == "BACK" and row.bet_on_team == winningTeam and row.bet_on_team != winningTeam:
-        return -row.amount
-    elif row.status == "LAY" and row.bet_on_team == winningTeam and row.bet_on_team == winningTeam:
-        return row.amount * - row.odds + row.amount
-    elif row.status == "LAY" and row.bet_on_team == winningTeam and row.bet_on_team != winningTeam:
-        return row.amount
+        if row.status == "BACK" and row.bet_on_team == winningTeam and row.winning_team == winningTeam:
+            return row.amount * row.odds - row.amount 
+        elif row.status == "BACK" and row.bet_on_team == winningTeam and row.bet_on_team != winningTeam:
+            return -row.amount
+        elif row.status == "LAY" and row.bet_on_team == winningTeam and row.bet_on_team == winningTeam:
+            return row.amount * - row.odds + row.amount
+        elif row.status == "LAY" and row.bet_on_team == winningTeam and row.bet_on_team != winningTeam:
+            return row.amount
     df['Loss/Profit'] = df.apply(CalculateLossProfit_userBetting,axis=1)
 
     return df
 
     
-
+# Betting.objects.update(
+#     loss_profit=Case(
+#         When(status="B",
+#              match="Srilanka vs India",
+#              winning_team = "Not declared yet",
+#              then=F("amount")*F("odds") - F("amount")),
+#         When(status="L",
+#              match="Srilanka vs India",
+#              winning_team = "Not declared yet",
+#              then=F("amount")* -F("odds") + F("amount")),
+#         default=F("loss_profit")
+# )
+#      )
 
