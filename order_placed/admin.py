c26ddf89758215c6076f424dc629981051289b3e
mayank <mayank.infograins@gmail.com>
2022-07-04 10:47:42 +0530

    add liveMatch field of Betting Views and admin side

--- order_placed/admin.py
+++ order_placed/admin.py
@@ -6,6 +6,6 @@ from .models import (
 
 class BettingAdmin(admin.ModelAdmin):
     model = Betting
-    list_display = ["id","user","amount","odds","status","bet_on_team","winning_team","loss_profit"]
+    list_display = ["id","user","amount","odds","status","bet_on_team","winning_team","match","loss_profit"]
     
 admin.site.register(Betting, BettingAdmin)
